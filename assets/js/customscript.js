c2aa54d22dce3816ffcd7d639df52075ba8be0e2
anuta5000 <anuta5000@bk.ru>
2015-11-16 01:40:53 +0300

    Create customscript.js

---
+++ assets/js/customscript.js
@@ -0,0 +1,15 @@
+(function ($) {
+  Drupal.behaviors.myFirstBehavior = {
+    attach : function(context, settings) {
+      
+		var selects_elem = $("select *");
+			
+			selects_elem.each(function () { 
+				
+				$(this).selectmenu();
+			});	
+		};
+		
+    }
+  };
+})(jQuery);
